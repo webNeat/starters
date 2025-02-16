8d2b41c235c3f1ce830d044e8829091c0f92caa1
Amine Ben hammou <webneat@gmail.com>
2025-02-16 21:43:34 +0100

    Make it an npm package

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Starters for personal projects
 
-```bash
-curl -fsSL https://raw.githubusercontent.com/webNeat/starters/refs/heads/master/init.mjs | node
-```
---
+++ starters.js
@@ -0,0 +1,10 @@
+export default {
+  "react-library": {
+    placeholders: {
+      "{{package-name}}": "Your package name, e.g. react-library",
+      "{{description}}": "A short description of your library",
+      "{{repo}}": "Your repository name, e.g. webneat/react-library",
+    },
+    templates: [".changeset/config.json", "package.json", "README.md"],
+  },
+};
