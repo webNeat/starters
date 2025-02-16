fdde74112590ea77df2dce5f7db69b7909422bb7
Amine Ben hammou <webneat@gmail.com>
2025-02-16 05:34:31 +0100

    new start

--- adonis-api/docker/db-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Creating database: ${TEST_POSTGRES_DB}"
-psql --username ${POSTGRES_USER} <<EOSQL
-CREATE DATABASE ${TEST_POSTGRES_DB} OWNER ${POSTGRES_USER};
-EOSQL
---
+++ init.mjs
@@ -0,0 +1,5 @@
+import { setTimeout } from "timers/promises";
+
+console.log("Hi");
+await setTimeout(3000);
+console.log("Bye!");
